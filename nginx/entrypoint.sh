55248e3cb1854fe25352568f3c693a2bf8f5d281
Dmitrii Larionov <mikrolux@gmail.com>
2024-12-23 16:19:01 +0700

    .

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,22 +4,6 @@
 DOMAIN=${DOMAIN:-db.peth.one}
 EMAIL=${EMAIL:-mikrolux@gmail.com}
 
-# Генерация базовой конфигурации nginx
-cat > /etc/nginx/conf.d/default.conf <<EOF
-server {
-    listen 80;
-    server_name $DOMAIN;
-    
-    location /.well-known/acme-challenge/ {
-        root /var/www/certbot;
-    }
-
-    location / {
-        return 301 https://\$host\$request_uri;
-    }
-}
-EOF
-
 # Получение сертификата
 certbot certonly --standalone \
     -d $DOMAIN \
@@ -27,28 +11,6 @@ certbot certonly --standalone \
     --agree-tos \
     --no-eff-email \
     --force-renewal
-
-# Генерация SSL конфигурации
-cat > /etc/nginx/conf.d/ssl.conf <<EOF
-server {
-    listen 443 ssl;
-    server_name $DOMAIN;
-
-    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
-
-    # Базовые SSL настройки
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_prefer_server_ciphers on;
-    ssl_ciphers HIGH:!aNULL:!MD5;
-
-    location / {
-        # Ваш основной контент
-        root /usr/share/nginx/html;
-        index index.html;
-    }
-}
-EOF
 
 # Запуск cron для автообновления сертификатов
 # Запускаем каждые 12 часов
